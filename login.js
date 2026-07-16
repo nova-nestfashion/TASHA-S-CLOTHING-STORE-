1c49159df8811b13d1443d1c0a961475371b3a33
nova-nestfashion <mweembasteward56@gmail.com>
2026-07-16 04:03:25 +0200

    Replace login.js with local authentication logic

--- login.js
+++ login.js
@@ -1,29 +1,29 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+window.login = function(){
+
+    const username =
+    document.getElementById("username").value;
+
+    const password =
+    document.getElementById("password").value;
 
 
-document.getElementById("loginBtn").addEventListener("click", function(){
+    if(
+        username === "tashaadmin" &&
+        password === "Tasha2026"
+    ){
 
-    alert("Login button pressed");
-
-    const email = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-
-    signInWithEmailAndPassword(auth, email, password)
-
-    .then((result)=>{
-
-        alert("Welcome Admin");
+        localStorage.setItem(
+            "adminLoggedIn",
+            "true"
+        );
 
         window.location.href = "admin.html";
 
-    })
 
-    .catch((error)=>{
+    } else {
 
-        alert("Error: " + error.message);
+        alert("Wrong username or password");
 
-    });
+    }
 
-});
+}
